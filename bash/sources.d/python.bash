96d1b52e95a9b52c530b258f7098784f0fabd52b
thenrio <thierry.henrio@gmail.com>
2017-11-06 15:58:33 +0100

    ditch terrible systemis

--- bash/sources.d/python.bash
+++ bash/sources.d/python.bash
@@ -5,12 +5,5 @@ export PYTHONSTARTUP=~/.python/pythonrc
 export WORKON_HOME=${HOME}/.virtualenvs
 export PROJECT_HOME=${HOME}/src/python
 
-# XXX assumes nux is deb 
-#
-if systemis nux && test $( dpkg -l virtualenvwrapper 2> /dev/null | grep '^ii' | wc -l ) == 1; then
-  . /etc/bash_completion.d/virtualenvwrapper
-else
-  # installed by pip
-  . /usr/local/bin/virtualenvwrapper.sh
-fi
-
+# installed by pip
+[ -f /usr/local/bin/virtualenvwrapper.sh ] && . /usr/local/bin/virtualenvwrapper.sh
