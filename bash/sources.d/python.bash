fe12cfa5c3f124f2399917b6bf7dddba44f31489
thenrio <thierry.henrio@gmail.com>
2020-09-03 09:57:49 +0200

    Modernize while upgrade kernel (5.7.0-3)
    virtualenvwrapper is for python2.
    python3 has support for them without additional package.

--- bash/sources.d/python.bash
+++ bash/sources.d/python.bash
@@ -1,9 +1,3 @@
 # -*- mode: bash -*-
 # vi: set ft=sh :
 export PYTHONSTARTUP=~/.python/pythonrc
-
-export WORKON_HOME=${HOME}/.virtualenvs
-export PROJECT_HOME=${HOME}/src/python
-
-# installed by pip
-[ -f /usr/local/bin/virtualenvwrapper.sh ] && . /usr/local/bin/virtualenvwrapper.sh
