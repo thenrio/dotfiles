875a575a60afcc461cff02dec7ea04395389850e
thenrio <thierry.henrio@gmail.com>
2025-03-19 14:54:52 +0100

    Remove duplicate path ~/bin

--- bash/sources.d/elixir.bash
+++
@@ -1,8 +0,0 @@
-# This one is ugly!
-# erlang and elixir packages are laggy on debian, both sid and erlang-solutions.
-# Alternates 
-# * use asdf? but I do not think I need to switch version, just bump...
-# * use precompiled package?
-# * use a link indirection (update-alternatives)
-# export PATH=/opt/src/elixir-lang/elixir/bin:$PATH
-# export PATH=/opt/elixir/1.15.6-25/bin:$PATH
